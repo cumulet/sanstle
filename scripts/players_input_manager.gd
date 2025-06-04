81b1c7be6f0a70c03ef83883b9097ef76969519c
guillaume <guillaumeppauli@gmail.com>
2025-06-04 14:32:56 +0200

    saved main scene and added controls for joining game

---
+++ scripts/players_input_manager.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("join"):
+		print(event.device)
+		print(Input.get_connected_joypads())
+		print(Input.get_joy_name(event.device))
